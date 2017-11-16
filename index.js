ca4aef9384a89fe90eee2563441eb75087aff2c8
tim-mawson <tim.mawson@uk.ibm.com>
2017-11-16 23:45:23 +0000

    Process static and image links

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const express = require("express");
-const request = require("request-promise-native");
-const crypto = require("crypto");
-const uniq = require("lodash/uniq");
+const express = require('express');
+const request = require('request-promise-native');
+const crypto = require('crypto');
+const uniq = require('lodash/uniq');
 
 const PORT = process.env.GOOGLEDOCIMAGES_PORT || 8000;
 
 const app = express();
 
 let simpleCache = {};
-const hash = crypto.createHash("sha256");
+const hash = crypto.createHash('sha256');
 
-app.get("/", (res, req) => {
+app.get('/', (res, req) => {
   const documentUrl = res.query.url;
   const urlHash = crypto
-    .createHash("sha256")
-    .update(documentUrl, "utf-8")
-    .digest("hex");
+    .createHash('sha256')
+    .update(documentUrl)
+    .digest('hex');
 
   if (simpleCache[urlHash]) {
     req.send(simpleCache[urlHash]);
   } else {
     request(documentUrl)
       .then(res => {
-        simpleCache[urlHash] = res;
-
-        const processedDocument = processDocument(res);
-
+        const processedDocument = processDocument(documentUrl, res);
+        simpleCache[urlHash] = processedDocument;
         req.send(processedDocument);
       })
       .catch(reason => {
@@ -34,14 +32,49 @@ app.get("/", (res, req) => {
   }
 });
 
-function processDocument(body) {
-  let imageMatches = uniq(
-    body.match(
-      /\(\/\/images-docs-opensocial\.googleusercontent\.com\/gadgets\/proxy\?url=(.*?)&(.*?)\)/g
-    )
-  );
-  console.log(imageMatches);
+function processDocument(documentUrl, body) {
+  // Process static relative links
+  const staticLinkRe = /(?:href|src)='(.*?)'/g;
+  let staticLinkMatch;
+  while ((staticLinkMatch = staticLinkRe.exec(body)) !== null) {
+    body = body.replace(
+      staticLinkMatch[1],
+      'http://docs.google.com' + staticLinkMatch[1]
+    );
+  }
+
+  // Process image links
+  const imageLinkRe = /\(\/\/images-docs-opensocial\.googleusercontent\.com\/gadgets\/proxy\?url=(.*?)&(.*?)\)/;
+  let imageLinkMatch;
+  while ((imageLinkMatch = imageLinkRe.exec(body)) !== null) {
+    const conversionResult = convertImageLink(
+      imageLinkMatch[0],
+      imageLinkMatch[1],
+      imageLinkMatch[2]
+    );
+    body = body.replace(conversionResult.source, conversionResult.target);
+  }
+
   return body;
 }
 
-app.listen(PORT, () => console.log("Listening on port", PORT));
+function convertImageLink(source, imageUrl, params) {
+  let imageWidth = /resize_w=([0-9]+)/.exec(params);
+  let imageHeight = /resize_h=([0-9]+)/.exec(params);
+
+  let imageWidthParam = imageWidth && '&width=' + imageWidth[1];
+  let imageHeightParam = imageHeight && '&height=' + imageHeight[1];
+
+  return {
+    source,
+    target:
+      //'(//localhost:3333/?url=' +
+        '(//resize.sonofmaw.co.uk/?url=' +
+        imageUrl +
+        (imageWidthParam ||
+      '') + (imageHeightParam ||
+      '') + ')'
+  };
+}
+
+app.listen(PORT, () => console.log('Listening on port', PORT));
