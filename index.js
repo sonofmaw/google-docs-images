0bf3432c5c0181af051cd2219c861520c9f5c389
tim-mawson <tim.mawson@uk.ibm.com>
2017-11-18 14:47:09 +0000

    Intinstate cache, with timed cleaning

--- index.js
+++ index.js
@@ -7,17 +7,48 @@ const PORT = process.env.GOOGLEDOCIMAGES_PORT || 8000;
 
 const app = express();
 
+let simpleCache = {};
+const hash = crypto.createHash('sha256');
+
+const MINUTES = 60 * 1000;
+
+function cleanCache() {
+  Object.keys(simpleCache).forEach(key => {
+    const entry = simpleCache[key];
+    if (Date.now() - entry.timestamp > 5 * MINUTES) {
+      delete simpleCache[key];
+    }
+  });
+}
+
+app.use(require('express-status-monitor')());
+
 app.get('/', (res, req) => {
   const documentUrl = res.query.url;
+  const urlHash = crypto
+    .createHash('sha256')
+    .update(documentUrl)
+    .digest('hex');
 
-  request(documentUrl)
-    .then(res => {
-      const processedDocument = processDocument(documentUrl, res);
-      req.send(processedDocument);
-    })
-    .catch(reason => {
-      req.status(reason.statusCode).send(reason.message);
-    });
+  if (simpleCache[urlHash]) {
+    req.send(simpleCache[urlHash].content);
+
+    cleanCache();
+  } else {
+    request(documentUrl)
+      .then(res => {
+        const processedDocument = processDocument(documentUrl, res);
+        req.send(processedDocument);
+
+        simpleCache[urlHash] = {
+          timestamp: Date.now(),
+          content: processDocument
+        };
+      })
+      .catch(reason => {
+        req.status(reason.statusCode).send(reason.message);
+      });
+  }
 });
 
 function processDocument(documentUrl, body) {
@@ -35,12 +66,13 @@ function processDocument(documentUrl, body) {
   const imageLinkRe = /\(\/\/images-docs-opensocial\.googleusercontent\.com\/gadgets\/proxy\?url=(.*?)&(.*?)\)/;
   let imageLinkMatch;
   while ((imageLinkMatch = imageLinkRe.exec(body)) !== null) {
+    const source = imageLinkMatch[0];
     const conversionResult = convertImageLink(
-      imageLinkMatch[0],
+      source,
       imageLinkMatch[1],
       imageLinkMatch[2]
     );
-    body = body.replace(conversionResult.source, conversionResult.target);
+    body = body.replace(source, conversionResult);
   }
 
   // Add a signature
@@ -60,16 +92,13 @@ function convertImageLink(source, imageUrl, params) {
   let imageWidthParam = imageWidth && '&width=' + imageWidth[1];
   let imageHeightParam = imageHeight && '&height=' + imageHeight[1];
 
-  return {
-    source,
-    //'(//localhost:3333/?url=' +
-    target:
-      '(//resize.sonofmaw.co.uk/?url=' +
-      imageUrl +
-      (imageWidthParam || '') +
-      (imageHeightParam || '') +
-      ')'
-  };
+  return (
+    '(//resize.sonofmaw.co.uk/?url=' +
+    imageUrl +
+    (imageWidthParam || '') +
+    (imageHeightParam || '') +
+    ')'
+  );
 }
 
 app.listen(PORT, () => console.log('Listening on port', PORT));
